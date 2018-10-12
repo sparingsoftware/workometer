33c788f08b99481cbf8e17207656d1d261b4c8d6
grzch <fangitary@gmail.com>
2018-10-12 18:03:51 +0200

    Initial commit with boilerplate

---
+++ workometer/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'workometer'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
