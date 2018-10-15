85db69ad96de78225ae1e9ebc32073bce4e8ddd4
vladprosolupov <prosolupov97@gmail.com>
2018-10-15 22:50:47 +0200

    Styling login form and enabling login on enter hit

--- workometer/src/renderer/components/LoginPage/Index.vue
+++ workometer/src/renderer/components/LoginPage/Index.vue
@@ -1,59 +1,71 @@
 <template>
   <div class="wrapper">
-    <el-form :model="form" ref="form" @submit.native.prevent="submitForm('form')">
+    <el-form :model="form" ref="form" @submit.native.prevent>
       <el-form-item
         prop="username"
         label="Username"
         :rules="rules.username"
       >
-        <el-input v-model="form.username"/>
+        <el-input v-model="form.username" @keyup.enter.native.prevent="submitForm('form')"/>
       </el-form-item>
       <el-form-item
         prop="password"
         label="Password"
         :rules="rules.password"
       >
-        <el-input v-model="form.password" type="password"/>
+        <el-input v-model="form.password" type="password" @keyup.enter.native.prevent="submitForm('form')"/>
       </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('form')">Login</el-button>
+      <el-form-item class="centered-content">
+        <el-button type="primary" @click="submitForm('form')" :loading="isFormSending">Login</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        form: {
-          username: '',
-          password: ''
-        },
-        rules: {
-          username: [
-            {required: true, message: 'Please input username', trigger: 'blur'}
-          ],
-          password: [
-            {required: true, message: 'Please input password', trigger: 'blur'}
-          ]
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: 'Please input username', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: 'Please input password', trigger: 'blur' }
+        ]
+      },
+      isFormSending: false
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // Enable loading animation
+          this.isFormSending = true
+          // Dispatching event to the store
+          this.$store.dispatch('login', this.form).then(() => {
+            // Disabling loading animation
+            this.isFormSending = false
+            // Redirecting to the dashboard
+            this.$router.push({ name: 'dashboard' })
+          }).catch(err => {
+            // Handling errors
+            this.handleErrors(err)
+            // Disabling loading animation
+            this.isFormSending = false
+          })
+        } else {
+          return false
         }
-      }
-    },
-    methods: {
-      submitForm (formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.$store.dispatch('login', this.form).then(response => {
-              this.$router.push({name: 'dashboard'})
-            }).catch(this.handleErrors)
-          } else {
-            return false
-          }
-        })
-      }
+      })
     }
   }
+}
 </script>
 <style lang="scss" scoped>
   .wrapper {
@@ -63,5 +75,11 @@
     transform: translate(-50%, -50%);
     top: 50%;
     left: 50%;
+  }
+
+  .centered-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
