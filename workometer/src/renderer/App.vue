4efd630f6f19533aebb16c3bd02ff7cba23cb9c8
vladprosolupov <prosolupov97@gmail.com>
2018-10-15 23:01:50 +0200

    Styling transition on router view

--- workometer/src/renderer/App.vue
+++ workometer/src/renderer/App.vue
@@ -1,14 +1,47 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition :name="slideDir" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
   export default {
+    computed: {
+      isUserAuth () {
+        return this.$store.getters.isUserAuth
+      },
+      slideDir () {
+        return this.isUserAuth ? 'slide-left' : 'slide-right'
+      }
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   /* CSS */
+  // slide left right animation
+  .slide-right-leave-active,
+  .slide-right-enter-active,
+  .slide-left-leave-active,
+  .slide-left-enter-active {
+    transition: transform 0.3s;
+  }
+
+  .slide-right-enter {
+    transform: translate(-100vw, 0);
+  }
+
+  .slide-right-leave-to {
+    transform: translate(100vw, 0);
+  }
+
+  .slide-left-enter {
+    transform: translate(100vw, 0);
+  }
+
+  .slide-left-leave-to {
+    transform: translate(-100vw, 0);
+  }
 </style>
