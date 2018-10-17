993e25abbb742a877c7af653e41741a16a2abc75
argeento <adrian.wieprzkowicz@gmail.com>
2018-10-17 16:43:53 +0200

    Refactor Jira initialization

--- src/renderer/jira.js
+++ src/renderer/jira.js
@@ -1,9 +1,9 @@
 import JiraClient from 'jira-connector'
 import store from '@/store'
 
-export default store.state.login.basic_auth ? new JiraClient({
+const auth = store.state.login.basic_auth
+
+export default new JiraClient({
   host: 'panowie.atlassian.net',
-  basic_auth: store.state.login.basic_auth
-}) : new JiraClient({
-  host: 'panowie.atlassian.net'
+  ...auth && { basic_auth: auth }
 })
