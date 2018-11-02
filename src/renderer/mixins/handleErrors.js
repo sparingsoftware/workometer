fa07de8afe4ad36ca192f9f0de3292765536d3dd
grzch <fangitary@gmail.com>
2018-11-02 18:02:22 +0100

    Newlines in errors

--- src/renderer/mixins/handleErrors.js
+++ src/renderer/mixins/handleErrors.js
@@ -3,13 +3,17 @@ export default {
     handleErrors (errors) {
       let formattedErrors = ''
       const parsedErrors = JSON.parse(errors)
-      formattedErrors += parsedErrors.body.errorMessages.join('\n')
+      formattedErrors += parsedErrors.body.errorMessages.join('<br>')
+      if (formattedErrors) {
+        formattedErrors += '<br>'
+      }
       Object.keys(parsedErrors.body.errors).forEach(field => {
-        formattedErrors += `${field}: ${parsedErrors.body.errors[field]}`
+        formattedErrors += `${field}: ${parsedErrors.body.errors[field]}<br>`
       })
       this.$notify({
         title: 'Error',
         message: formattedErrors,
+        dangerouslyUseHTMLString: true,
         type: 'error'
       })
     }
