932794fb8e0e487b962d1fff55fe233cf6096ce0
grzch <fangitary@gmail.com>
2018-11-02 15:46:54 +0100

    Remove unnecessary console.log

--- src/renderer/mixins/handleErrors.js
+++ src/renderer/mixins/handleErrors.js
@@ -1,7 +1,6 @@
 export default {
   methods: {
     handleErrors (errors) {
-      console.log(errors, 'errors')
       const parsedErrors = JSON.parse(errors).body.errorMessages
       this.$notify({
         title: 'Error',
