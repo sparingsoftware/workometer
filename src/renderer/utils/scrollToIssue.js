f849954eb6d1942cb229b90bd52728cf7ca48f59
Vladyslav Prosolupov <vladyslavprosolupov@gmail.com>
2018-12-04 18:22:26 +0100

    Update src/renderer/utils/scrollToIssue.js
    Co-Authored-By: grzch <szymborg@ee.pw.edu.pl>

--- src/renderer/utils/scrollToIssue.js
+++ src/renderer/utils/scrollToIssue.js
@@ -1,4 +1,4 @@
-export default (issue, opts = {}) => {
+export default (issue, opts = { behavior: 'smooth' }) => {
   if (!issue) return
   const issueEl = document.getElementById(issue.key)
   if (issueEl) {
